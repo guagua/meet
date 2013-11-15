c00757946a756988a2c72152c4b8eb89b90d1154
maxing <maxing@baidu.com>
2013-11-15 19:19:19 +0800

    add PCSUtil.java

---
+++ src/com/baidu/meet/util/PCSUtil.java
@@ -0,0 +1,25 @@
+package com.baidu.meet.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.baidu.oauth.BaiduOAuth;
+import com.baidu.oauth.BaiduOAuth.BaiduOAuthResponse;
+import com.baidu.pcs.BaiduPCSClient;
+import com.baidu.pcs.BaiduPCSErrorCode;
+import com.baidu.pcs.BaiduPCSStatusListener;
+import com.baidu.pcs.BaiduPCSActionInfo;
+import android.os.Handler;
+import com.baidu.meet.log.MeetLog;
+
+/**
+ * 网盘PCS辅助类
+ * 
+ * @author 
+ * 
+ */
+public class PCSUtil {
+    private static String mOauth = null;
+    private final static String mApiKey = "Uw8BvIB211QFFs87TaqR9mkU";
+    private final static String mRootPath = "/duapp/loveface-dev"
+
+}
